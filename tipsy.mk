da346bcd91a2cf222bb73100a682787ae02a6af5
premaca <premaca@gmail.com>
2017-06-04 18:13:37 +0200

    tipsy.mk: don't override product_model plz.
    Change-Id: I0921f1716a546aeff1e62cd8b048d26f7f67a2ae

--- tipsy.mk
+++ tipsy.mk
@@ -26,8 +26,6 @@ PRODUCT_NAME := tipsy_addison
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=motorola/addison/addison:7.0/NPN25.137-24-1/1:user/release-keys \
     PRIVATE_BUILD_DESC="addison-user 7.0 NPN25.137-24-1 1 release-keys" \
