c632fc5b0b8ec4b8ef1526c7d536ee5118e8df00
Alberto97 <albertop2197@gmail.com>
2017-07-26 09:56:47 +0200

    addison: NPN26.118-22 Release
    Change-Id: I9d36d4c2de7acbcc3a918e00f99788a9287727f9

--- gzosp.mk
+++ gzosp.mk
@@ -28,6 +28,6 @@ PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=motorola/addison/addison:7.1.1/NPN26.107/22:user/release-keys \
-    PRIVATE_BUILD_DESC="addison-user 7.1.1 NPN26.107 22 release-keys" \
+    BUILD_FINGERPRINT=motorola/addison/addison:7.1.1/NPN26.118-22/21:user/release-keys \
+    PRIVATE_BUILD_DESC="addison-user 7.1.1 NPN26.118-22 21 release-keys" \
     PRODUCT_NAME="Moto Z Play"
