c1d6f50045ecd43d93319a303c5dfd78a8629f58
Vachounet <vachounet@live.fr>
2017-08-23 13:59:03 +0200

    Addison: remove sepolicies for now
    Change-Id: Ib56069e29b9987f42f9c2083e8a37e73778e7ee2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,8 +137,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
