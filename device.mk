8927888de35ee61053c80018befa0f1936be3b9e
Vachounet <vachounet@live.fr>
2017-08-23 12:58:38 +0200

    Addison: Update display/render for O
    Change-Id: I34029ba9d9c1d6163c3c8f495fb9d4867cb0b040
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -122,10 +122,19 @@ PRODUCT_COPY_FILES += \
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8953 \
-    copybit.msm8953 \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
+    libgenlock \
     libtinyxml
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # Display Calibration
 PRODUCT_PACKAGES += \
