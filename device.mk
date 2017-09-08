475173c25ccb2018d85b0f035b72c8f05f506393
LuK1337 <priv.luk@gmail.com>
2017-09-08 13:46:20 +0200

    Addison: add basic 2.0 -> 2.1 fingerprint HAL wrapper
    Change-Id: I27963bfb1402aba10e4ceba669520b630d075abf

--- device.mk
+++ device.mk
@@ -163,7 +163,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    fingerprint.msm8953
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
